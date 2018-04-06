fd49e4fd258e34d37480b3695fd8f958f53934b7
vsidhu42 <simplyxvickx@gmail.com>
2018-04-06 19:49:42 +0000

    Added validation to models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
   has_many :foods
+
+  validates :name, presence: true
+
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,10 @@
 class Food < ApplicationRecord
   belongs_to :category
   has_many :food_orders
+
+  validates :name, presence: true
+  validates :quantity, presence: true
+  validates :desc, presence: true
+  validates :cost, numericality: true, presence: true
+
 end
